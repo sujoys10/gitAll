b87753f1b89d6b1b1f6653e0c648299374701efe
Sujoy Saha <sujoys023@gmail.com>
2021-02-16 11:43:43 +0530

    formatted

--- src/redux/action/gallery.js
+++ src/redux/action/gallery.js
@@ -1,22 +1,27 @@
-const { GET_PHOTOS_PENDING, GET_PHOTOS_FAILED, GET_PHOTOS_SUCCESS } = require("../actionTypes");
+const {
+  GET_PHOTOS_PENDING,
+  GET_PHOTOS_FAILED,
+  GET_PHOTOS_SUCCESS,
+} = require("../actionTypes");
 
-export const fetchPhotos = (searchTerm, page) => async(dispatch) => {
-    dispatch({ type: GET_PHOTOS_PENDING})
-    try{
-        const res = await fetch(`https://api.unsplash.com/search/photos/?query=${searchTerm}&page=${page}&per_page=4`,{
-            headers: {
-                Authorization: "Client-ID haIRKtGI1nIVMEkATMeAX_YR6PFZzNMjhYfPoS3wK_I"
-            }
-        })
-        const data = await res.json();
-        dispatch({
-            type: GET_PHOTOS_SUCCESS,
-            payload: data
-        })
-    }catch(err){
-        dispatch({ type: GET_PHOTOS_FAILED})
-    }
-} 
-
-
-
+export const fetchPhotos = (searchTerm, page) => async (dispatch) => {
+  dispatch({ type: GET_PHOTOS_PENDING });
+  try {
+    const res = await fetch(
+      `https://api.unsplash.com/search/photos/?query=${searchTerm}&page=${page}&per_page=4`,
+      {
+        headers: {
+          Authorization:
+            "Client-ID haIRKtGI1nIVMEkATMeAX_YR6PFZzNMjhYfPoS3wK_I",
+        },
+      }
+    );
+    const data = await res.json();
+    dispatch({
+      type: GET_PHOTOS_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({ type: GET_PHOTOS_FAILED });
+  }
+};
