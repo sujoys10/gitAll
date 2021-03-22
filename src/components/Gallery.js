2a60bdb54a08b2df8a648bdafcf66af0b85078c1
Sujoy Saha <sujoys023@gmail.com>
2021-03-22 09:27:35 +0530

    conflict3 testing

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import ImageCard from './ImageCard';
 
 const Gallery = ({images}) => {
-
+    <fiv> lrets do it</fiv>
     return (
         <Fragment>
             { images.map((img,i) => (
